c333d9c65eb1a14ebe7a9967b9911bba01c76d8d
Gaëtan André <rvlander@gaetanandre.eu>
2019-09-05 14:19:04 +0200

    update site

---
+++ faq.markdown
@@ -0,0 +1,14 @@
+---
+title: FAQ
+---
+
+# Man, this site is so hugly. Aren't you ashamed?
+If you think you can de better, email me. I might share donations with you.
+
+# Shouldn't you have helped make staticman better instead of making cyment?
+I wanted to have fun with Haskell.
+
+# Do you plan to support `staticman.yml` configuration file?
+Currently what I have done is geared towards my needs. If you are missing a feature post an issue.
+
+
--- site.hs
+++ site.hs
@@ -1,7 +1,10 @@
 --------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+
 import           Data.Monoid (mappend)
 import           Hakyll
+import Data.String.Interpolate (i)
 
 
 --------------------------------------------------------------------------------
@@ -15,53 +18,31 @@ main = hakyll $ do
         route   idRoute
         compile compressCssCompiler
 
-    match (fromList ["about.rst", "contact.markdown"]) $ do
+    match (fromList ["about.markdown", "contact.markdown"]) $ do
         route   $ setExtension "html"
         compile $ pandocCompiler
             >>= loadAndApplyTemplate "templates/default.html" defaultContext
             >>= relativizeUrls
 
-    match "posts/*" $ do
-        route $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/post.html"    postCtx
-            >>= loadAndApplyTemplate "templates/default.html" postCtx
-            >>= relativizeUrls
-
-    create ["archive.html"] $ do
-        route idRoute
+    match "*.markdown" $ do
+        route $setExtension "html"
         compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let archiveCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Archives"            `mappend`
+            path <- getUnderlying
+            comments <- chronological =<< loadAll [i|comments/#{path}/*|]
+            let fullCtx =
+                    listField "comments" postCtx (return comments) `mappend`
                     defaultContext
 
-            makeItem ""
-                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
-                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
-                >>= relativizeUrls
-
-
-    match "index.html" $ do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let indexCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Home"                `mappend`
-                    defaultContext
-
-            getResourceBody
-                >>= applyAsTemplate indexCtx
-                >>= loadAndApplyTemplate "templates/default.html" indexCtx
+            pandocCompiler
+                >>= applyAsTemplate fullCtx
+                >>= loadAndApplyTemplate "templates/default.html" fullCtx
                 >>= relativizeUrls
 
     match "templates/*" $ compile templateBodyCompiler
-
 
 --------------------------------------------------------------------------------
 postCtx :: Context String
 postCtx =
     dateField "date" "%B %e, %Y" `mappend`
+    dateField "hour" "%R"`mappend`
     defaultContext
