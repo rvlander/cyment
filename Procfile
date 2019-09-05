da354c6840aff799217d7232240393cd5a0cae70
Gaëtan André <rvlander@gaetanandre.eu>
2019-09-05 14:51:18 +0200

    deploy to heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: cyment-exe
--- app/Main.hs
+++ app/Main.hs
@@ -16,18 +16,21 @@ import System.Environment (getEnv)
 main =
   let 
     idPattern = pack . concat $  replicate 8 "[0-9a-z]"
-  in scotty 3000 $
-   post "/:namespace/:repo" $ rescue (do
-     namespace <- param "namespace"
-     repo <- param "repo"
-     path <- param "path"
-     commenter <- param "commenter"
-     comment <- param "comment"
-    --  redirectUrl <- param "redirect" 
-     liftAndCatchIO $ do 
-        commentId <- stringRandomIO idPattern
-        datetime <- getCurrentTime
-        githubToken <- getEnv "GITHUB_TOKEN"
-        res <- pushCommitAndMakePR githubToken namespace repo commentId path commenter $ buildContent commenter comment datetime
-        either (putStrLn . show) (putStrLn . show) res
-     html "Thanks, your comment is waiting to be approved!") (\msg -> status badRequest400)
+  in 
+    do
+      githubToken <- getEnv "GITHUB_TOKEN"
+      port <- fromMaybe 3000 $ lookupEnv "PORT"
+    scotty 3000 $
+    post "/:namespace/:repo" $ rescue (do
+      namespace <- param "namespace"
+      repo <- param "repo"
+      path <- param "path"
+      commenter <- param "commenter"
+      comment <- param "comment"
+     --  redirectUrl <- param "redirect" 
+      liftAndCatchIO $ do 
+         commentId <- stringRandomIO idPattern
+         datetime <- getCurrentTime
+         res <- pushCommitAndMakePR githubToken namespace repo commentId path commenter $ buildContent commenter comment datetime
+         either (putStrLn . show) (putStrLn . show) res
+      html "Thanks, your comment is waiting to be approved!") (\msg -> status badRequest400)
